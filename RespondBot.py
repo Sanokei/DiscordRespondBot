0d536b4eb65e944fd0c3354f2904f438b188feff
NobreHD <60514067+NobreHD@users.noreply.github.com>
2022-03-04 01:46:13 +0000

    Major Update

--- RespondBot.py
+++ RespondBot.py
@@ -1,67 +1,124 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-
-import config
+from profanity_filter import ProfanityFilter
+from configparser import ConfigParser
+from nextcord.ext import commands
+from spacy.lang.en import English
 import openai
+import random
 
-openai.api_key = config.api_key
-token = config.token
-all_text = ""
-last_four_responses = []
-#login
-bot = discord.Client()
-bot = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
+pf = ProfanityFilter()
+message_history = {}
+npl = English()
+sbd = npl.create_pipe("sentencizer")
+npl.add_pipe(sbd)
 
-@bot.event
-async def on_ready():
-    print("Logged in as: " + bot.user.name)
-    print("ID: " + str(bot.user.id))
-    print('------')
+config = ConfigParser()
+config.read('config.cfg')
 
-@bot.command(aliases=['convo','conversation'], name="Convo",description="Sends the current conversation")
-async def convo(ctx):
-    # reply as file
-    global all_text
-    with open("convo.txt", "w+") as f:
-        f.write(all_text)
-    await ctx.send(file=discord.File("convo.txt"))
+openai.api_key = config['keys']['openai']
+token = config['keys']['discord']
+bot_name = config['bot']['name']
 
-@bot.event
-async def on_message(message):
-    global all_text
-    global last_four_responses
-    if message.author.bot:
-        return
-    if bot.user.mentioned_in(message):
-        t = message.content.replace(bot.user.mention, '')
-        if(">" in t and "<" in t and "@" in t):
-            text = t.split(">")[1]
+def scroll_array(new, channel):
+    #Adds the new message to the beginning and removes the last message from the array
+    global message_history
+    history = get_history_by_channel(channel)
+    history.insert(0,new)
+    history.pop()
+    message_history[channel] = history
+
+def remove_empty_strings(array: list):
+    #Removes empty strings from the array
+    temp = array.copy()
+    while "" in temp:
+        temp.remove("")
+    return temp
+
+def get_formatted_chat(message, ctx):
+    output = ""
+    history = remove_empty_strings(get_history_by_channel(ctx.channel.id))
+    
+    #Change between bot and user
+    for i in range(len(history)):
+        if i%2 == 0:
+            output = f"\n{bot_name}: {history[i]}" + output
         else:
-            text = t
-        all_text += "\nFriend:" + text
-        last_four_responses.append("Friend: "+text)
-        if(len(last_four_responses) > 4):
-            last_four_responses.pop(0)
-        a = ""
-        for i in last_four_responses:
-            a += i + "\n"
-        response = openai.Completion.create(
-            engine="text-davinci-001",
-            prompt=f"This is a conversation between an AI named Human and a Friend.\n{config.prompt}\n{a}\nFriend:{text}\nHuman:",
-            temperature=0.9,
-            max_tokens=300,
-            top_p=0.5,
-            frequency_penalty=0.3,
-            presence_penalty=0.5,
-            stop=["Friend:","Human:"]
-        )
-        all_text += "\nHuman:" + response.choices[0].text.replace("\n","")
-        last_four_responses.append("Human:"+response.choices[0].text.replace("\n",""))
-        await message.reply(response.choices[0].text)
+            output = "\n" + history[i] + output
+            
+    if not message == "":
+        output += f"\n{ctx.author.name}: {message}\n{bot_name}: "
+        
+    return output
+
+def get_history_by_channel(channel):
+    if channel in message_history:
+        return message_history[channel]
     else:
-        await bot.process_commands(message)  
-bot.run(config.token)
+        return [""]*10
+
+@client.event
+async def on_connect():
+    print(f"""
+        Client has successfully logged in as {client.user.name}#{client.user.discriminator}
+        Your discord ID is {client.user.id}
+    """)
+    
+@client.command()
+async def history(ctx):
+    message = get_formatted_chat("", ctx)
+    if message == "":
+        message = "No memories yet"
+    await ctx.send(f"```{message}```")
+    
+@client.command()
+async def reset(ctx):
+    global message_history
+    message_history[ctx.channel.id] = [""] * len(get_formatted_chat(ctx.channel.id))
+    await ctx.send("Memories reset")
+    
+@client.command()
+async def memory(ctx, limit):
+    global message_history
+    difference = int(limit) - len(get_formatted_chat(ctx.channel.id))
+    if difference > 0:
+        message_history[ctx.channel.id].append([""] * difference)
+    elif difference < 0:
+        message_history[ctx.channel.id] = message_history[ctx.channel.id][:int(limit)]
+    
+    await ctx.send(f"Memory limit set to {len(message_history[ctx.channel.id])}")
+    
+@client.command()
+async def message(ctx, *, message):
+    #Fake Typing Effect
+    ctx.typing() 
+
+    print(f"{ctx.author.name}#{ctx.author.discriminator} said: {message}")
+
+    censored_message = pf.censor(message)
+    if censored_message != message:
+        print("Got censored")
+        message = censored_message
+    
+    temperature = random.uniform(0.8, 1.0)
+    response = openai.Completion.create(
+        engine="text-davinci-001",
+        prompt=f"The following is a conversation with a group of friends. \n{get_formatted_chat(message, ctx)}",
+        temperature=temperature,
+        max_tokens=300,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[f" {ctx.author.name}:", f" {bot_name}:"]
+    )
+    
+    doc = npl(response.choices[0].text)
+    sents = [sent.string.strip() for sent in doc.sents]
+    
+    scroll_array(f"{ctx.author.name}: {message}", ctx.channel.id)
+    scroll_array(sents[0], ctx.channel.id)
+    await ctx.reply(response.choices[0].text)
+    
+client.run(token)
